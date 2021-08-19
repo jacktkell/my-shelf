61d8688ca15b207b4c6e2ab829ef2711d9103f32
jack <jack.kelling7@gmail.com>
2021-08-19 08:41:25 -0500

    App.js updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
+import BookCollection from "./components/BookCollection";
+import LoginSignupPage from "./components/LoginSignupPage";
+import BookDetails from "./components/BookDetails";
+import MyShelf from "./components/MyShelf";
 
 function App() {
+  const [currentUser, setCurrentUser] = useState({});
+
+  function storeUser(user) {
+    localStorage.setItem("user", user.id);
+    setCurrentUser(user);
+  }
+
+  function handleLogout() {
+    async function logout() {
+      const res = await fetch("/logout", { method: "DELETE" });
+      if (res.ok) {
+        localStorage.removeItem("user");
+        setCurrentUser({});
+      }
+    }
+    logout();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        {localStorage.getItem("user") && (
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/myshelf">My Shelf</Link>
+              </li>
+              <li>
+                <button onClick={handleLogout}>Log out</button>
+              </li>
+            </ul>
+          </nav>
+        )}
+        <Switch>
+          {localStorage.getItem("user") ? (
+            <>
+              <Route path="/books/:id">
+                <BookDetails currentUser={currentUser} />
+              </Route>
+              <Route path="/" exact>
+                <BookCollection currentUser={currentUser} />
+              </Route>
+              <Route path="/myshelf">
+                <MyShelf currentUser={currentUser} />
+              </Route>
+            </>
+          ) : (
+            <Route exact path="/">
+                <LoginSignupPage
+                  onLogin={storeUser}
+                  currentUser={currentUser}
+                />
+            </Route>
+          )}
+          <Redirect to="/" />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
